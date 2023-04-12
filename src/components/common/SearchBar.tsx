6974a0c1ecc9dcf00a3deb8b30484a7318b31712
callmejeje <choije401@naver.com>
2023-04-12 16:49:46 +0900

    style: center로 정렬

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -2,7 +2,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchBar = () => {
   return (
-    <div className="flex border rounded w-full p-2">
+    <div className="flex items-center border rounded w-full p-2">
       <AiOutlineSearch size="24" />
       <input className="w-full outline-0 ml-2" />
     </div>
