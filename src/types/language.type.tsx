a66f4bb8fedffe022669dfda05148f6608e37c76
callmejeje <choije401@naver.com>
2023-04-13 14:42:59 +0900

    feat: 언어 및 프레임워크 종류 추가

--- src/types/language.type.tsx
+++ src/types/language.type.tsx
@@ -1,25 +1,43 @@
 import { ReactElement } from "react";
 import JavaScriptSvg from "../assets/images/languages/javascript.svg";
+import NodeJSSvg from "../assets/images/languages/nodejs.svg";
+import ReactSVG from "../assets/images/languages/react.svg";
 import TypeScriptSvg from "../assets/images/languages/typescript.svg";
 
 export interface Language {
+  id: number;
   name: string;
   icon: ReactElement;
 }
 
 export type LanguageList = {
   language: Language[];
+  framework: Language[];
 };
 
 export const languageList: LanguageList = {
   language: [
     {
+      id: 0,
       name: "JS",
       icon: <img src={JavaScriptSvg} />,
     } as Language,
     {
+      id: 1,
       name: "TS",
       icon: <img src={TypeScriptSvg} />,
     } as Language,
+  ],
+  framework: [
+    {
+      id: 0,
+      name: "react",
+      icon: <img src={ReactSVG} />,
+    },
+    {
+      id: 1,
+      name: "nodeJS",
+      icon: <img src={NodeJSSvg} />,
+    },
   ],
 };
