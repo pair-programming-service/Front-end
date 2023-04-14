7b44681748a72e352f9cab5e309c1747ed668459
callmejeje <choije401@naver.com>
2023-04-14 16:33:14 +0900

    feat: 페어 모집 글 목록 서버 데이터 연결

--- src/components/pairPostList/PostList.tsx
+++ src/components/pairPostList/PostList.tsx
@@ -1,30 +1,12 @@
 import PostCard from "components/common/PostCard";
-import { languageList } from "types/language.type";
+import { PairPost } from "types/post.type";
 
-const PostList = () => {
+const PostList = ({ postList }: { postList: PairPost[] }) => {
   return (
     <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-4">
-      <PostCard
-        languages={[
-          languageList.language[0],
-          languageList.language[1],
-          languageList.language[1],
-          languageList.language[1],
-          languageList.language[1],
-        ]}
-      />
-      <PostCard
-        languages={[languageList.language[0], languageList.language[1]]}
-      />
-      <PostCard
-        languages={[languageList.language[0], languageList.language[1]]}
-      />
-      <PostCard
-        languages={[languageList.language[0], languageList.language[1]]}
-      />
-      <PostCard
-        languages={[languageList.language[0], languageList.language[1]]}
-      />
+      {postList.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </div>
   );
 };
