83ee70743ac784e754ac8042d7a57400af25e62f
callmejeje <choije401@naver.com>
2023-04-14 17:54:01 +0900

    feat: 속성명 변경

--- src/types/filter.type.tsx
+++ src/types/filter.type.tsx
@@ -3,20 +3,20 @@ import ProgramTypeSelect from "components/common/ProgramTypeSelect";
 import { ReactElement } from "react";
 
 export interface FilterOption {
-  optionNum: number;
-  optionName: string;
+  id: number;
+  name: string;
   selectComponent: ReactElement;
 }
 
 export const optionList: FilterOption[] = [
   {
-    optionNum: 0,
-    optionName: "언어 및 프레임워크",
+    id: 0,
+    name: "언어 및 프레임워크",
     selectComponent: <LanguageSelect />,
   },
   {
-    optionNum: 1,
-    optionName: "프로그래밍 유형",
+    id: 1,
+    name: "프로그래밍 유형",
     selectComponent: <ProgramTypeSelect />,
   },
 ];
