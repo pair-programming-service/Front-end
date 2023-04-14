0d45e6c1d07b743a30d7b6f5ba5a68ba67312a0d
callmejeje <choije401@naver.com>
2023-04-14 16:30:11 +0900

    feat: api 인스턴스 생성 및 요청 함수 구현

---
+++ src/apis/pairpost.ts
@@ -0,0 +1,28 @@
+import { PairPost } from "types/post.type";
+import request from "./base";
+
+export const getPairPostList = (
+  page: number,
+  size: number,
+  searchValue: string
+) => {
+  return request({
+    url: `/board/all?page=${page}&size=${size}&search=${searchValue}`,
+  });
+};
+
+export const postPairPost = (data: PairPost) => {
+  return request({
+    method: "POST",
+    url: `/board`,
+    data,
+  });
+};
+
+export const editPairPost = (data: PairPost, id: number) => {
+  return request({
+    method: "PUT",
+    url: `/board/${id}`,
+    data,
+  });
+};
