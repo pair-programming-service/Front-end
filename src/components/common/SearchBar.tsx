d8faab2f6da01680f533a1b4abbc2ab6571bcf54
callmejeje <choije401@naver.com>
2023-04-17 23:24:30 +0900

    refactor: 변수명 변경

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -2,19 +2,19 @@ import React, { SetStateAction } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchBar = ({
-  searchValue,
-  setSearchValue,
+  searchInput,
+  setSearchInput,
 }: {
-  searchValue: string;
-  setSearchValue: React.Dispatch<SetStateAction<string>>;
+  searchInput: string;
+  setSearchInput: React.Dispatch<SetStateAction<string>>;
 }) => {
   return (
     <div className="flex items-center border rounded w-full p-2">
       <AiOutlineSearch size="24" />
       <input
         className="w-full outline-0 ml-2"
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
       />
     </div>
   );
