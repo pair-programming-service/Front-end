2b93be5ed045c518aec413f1ddfb16b74442d3c0
callmejeje <choije401@naver.com>
2023-05-08 22:14:58 +0900

    feat: icon props 추가

--- src/components/common/SquareButton.tsx
+++ src/components/common/SquareButton.tsx
@@ -1,13 +1,16 @@
+import React, { ReactElement } from "react";
 import { ButtonStyle } from "types/styles.type";
 
 const SquareButton = ({
   text,
   handleClick,
   style,
+  icon,
 }: {
   text: string;
-  handleClick: () => void;
+  handleClick: (e: React.MouseEvent<HTMLElement>) => void | (() => void);
   style: ButtonStyle;
+  icon?: ReactElement;
 }) => {
   const textColor = style.isWhite ? "text-[#6A6A6A]" : "text-white";
   const bgColor = style.isWhite ? "bg-white" : "bg-cm-400";
@@ -15,9 +18,10 @@ const SquareButton = ({
 
   return (
     <button
-      className={`px-7 ${bgColor} border ${borderColor} h-12 rounded-lg ${textColor} font-bold text-base whitespace-nowrap`}
+      className={`relative px-7 ${bgColor} border ${borderColor} h-12 rounded-lg ${textColor} font-bold text-base whitespace-nowrap`}
       onClick={handleClick}
     >
+      <span>{icon !== undefined && icon}</span>
       {text}
     </button>
   );
