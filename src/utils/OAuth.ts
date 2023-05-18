a3f07ed45c7a9521673923a8cad502c9dbd6c1aa
hyeone999 <caps026@naver.com>
2023-05-18 20:44:41 +0900

    feat: 카카오 로그인 REDIRECT_URI .env 처리

--- src/utils/OAuth.ts
+++ src/utils/OAuth.ts
@@ -1,3 +1,3 @@
 const CLIENT_ID = process.env.REACT_APP_KAKAO_API_KEY;
-const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
+const REDIRECT_URI = process.env.REACT_APP_KAKAO_URL;
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
